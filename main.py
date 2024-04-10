8df15a8b3f7ca16b35b24fd3dc616216ee8266fc
gitwes <wesleyfreire1707@gmail.com>
2024-04-09 22:31:58 -0300

    inserindo os dados no contrato

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ planilha_fornecedores = load_workbook('./fornecedores.xlsx')
 pagina_fornecedores = planilha_fornecedores['Sheet1']
 
 for linha in pagina_fornecedores.iter_rows(min_row=2,values_only=True):
-    nome_empresa, endereco,cidade, estado, cep, telefone, email, setor = linha #unpacking
+    nome_empresa, endereco, cidade, estado, cep, telefone, email, setor = linha #unpacking
+  
+    arquivo_word = Document()
+    arquivo_word.add_heading('contrato de prestação de serviço',0)
